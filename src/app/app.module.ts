546960c2317be2d30d8cb48d2494081b94f18521
Edgar Arcos <edgararcosmartin@gmail.com>
2023-10-09 16:30:33 +0100

    Componetizando home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FotoPokemonComponent } from './components/foto-pokemon/foto-pokemon.component';
+import { TarjetaPokemonComponent } from './components/tarjeta-pokemon/tarjeta-pokemon.component';
+import { EstadisticasComponent } from './components/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FotoPokemonComponent,
+    TarjetaPokemonComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule,
